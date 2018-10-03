29b1813e6d2629a1d14ea1c9b723faea127fa046
micahoriaso <micahoriaso@gmail.com>
2018-10-03 13:54:00 +0300

    chore (app-setup): Setup react-router version 4

---
+++ src/views/Home/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home = () => <div>Welcome to the Authors' haven</div>;
+
+export default Home;
---
+++ src/views/Login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Login = () => <div>Welcome to the login page</div>;
+
+export default Login;
